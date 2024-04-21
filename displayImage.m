7d93cc38f2aa69c9c121e64d505ca0776361fd8f
fani <fani@mtmedia.org>
2024-04-21 22:53:03 +0200

    display instructions with images and text

---
+++ displayImage.m
@@ -0,0 +1,14 @@
+function displayImage(window, text, img, screenXpixels, screenYpixels)
+        texture = Screen('MakeTexture', window, img);
+        % Calculate the position of the scaled image to center it
+        dstX = (screenXpixels - screenXpixels * 0.7) / 2;
+        dstY = (screenYpixels - screenYpixels * 0.7) / 2;
+        dstRect = [dstX dstY dstX + screenXpixels * 0.7 dstY + screenYpixels * 0.7];
+        % Add text on top of the image
+        Screen('TextSize', window, 30);
+        DrawFormattedText(window, text, 'center', dstY * 0.5, 0);
+        % Draw the scaled texture to the window
+        Screen('DrawTexture', window, texture, [], dstRect);
+        %Screen('DrawTexture', window, texture, []);
+        Screen('Flip', window);
+end
