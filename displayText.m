b099db32c2269b37e97be7830228517dd081a41b
fani <fani@mtmedia.org>
2024-04-22 15:43:24 +0200

    removed static text size

--- displayText.m
+++ displayText.m
@@ -1,6 +1,6 @@
 function displayText(window, text)
     % Display text on the Psychtoolbox window
-    Screen('TextSize', window, 24);
+    %Screen('TextSize', window, text_size);
     DrawFormattedText(window, text, 'center', 'center');
     Screen('Flip', window);
 end
