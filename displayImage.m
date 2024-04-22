40f411a2c72a6655ef5f8f38f3cf4bfc3ead501a
fani <fani@mtmedia.org>
2024-04-22 15:42:42 +0200

    changed text height

--- displayImage.m
+++ displayImage.m
@@ -5,8 +5,8 @@ function displayImage(window, text, img, screenXpixels, screenYpixels)
         dstY = (screenYpixels - screenYpixels * 0.7) / 2;
         dstRect = [dstX dstY dstX + screenXpixels * 0.7 dstY + screenYpixels * 0.7];
         % Add text on top of the image
-        Screen('TextSize', window, 30);
-        DrawFormattedText(window, text, 'center', dstY * 0.5, 0);
+        %Screen('TextSize', window, 30);
+        DrawFormattedText(window, text, 'center', dstY * 0.8, 0);
         % Draw the scaled texture to the window
         Screen('DrawTexture', window, texture, [], dstRect);
         %Screen('DrawTexture', window, texture, []);
