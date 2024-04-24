585073f5efb7afc6929db24a90425c1a152d316a
fani <fani@mtmedia.org>
2024-04-24 12:34:45 +0200

    added jitter

--- generateInstructionPictures.m
+++ generateInstructionPictures.m
@@ -35,26 +35,33 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 
 % Define grid size
-numRows = 9;
-numCols = 12;
-cellWidth = screenXpixels / numCols;
-cellHeight = screenYpixels / numRows;
+num_rows = 9;
+num_cols = 12;
+cell_width = screenXpixels / num_cols;
+cell_height = screenYpixels / num_rows;
 
 % Define a line length based on the cell size
-lineLength = min(cellWidth, cellHeight) * 0.5;
+lineLength = min(cell_width, cell_height) * 0.5;
 lineWidth = 0.1 * lineLength; % Line width
+
+% Define jittter
+jitter_x = round(0.1*screenXpixels/num_cols);
+jitter_y = round(0.1*screenYpixels/num_rows);
 
 anglesMatrix = {simple_matrix, matrix};
 for a = 1:2
     % Loop through each cell and draw lines based on angles
-    for row = 1:numRows
-        for col = 1:numCols
-            % Calculate center of each cell
-            xCenter = (col - 0.5) * cellWidth;
-            yCenter = (row - 0.5) * cellHeight;
+    for row = 1:num_rows
+        for col = 1:num_cols
+
+            x_jitter = randi([-jitter_x, jitter_x]);
+            y_jitter = randi([-jitter_y, jitter_y]);
+
+            % Calculate center of each cell & add jitter
+            x_center = ((col - 0.5) * cell_width) + x_jitter;
+            y_center = ((row - 0.5) * cell_height)+ y_jitter;
 
             % Retrieve angles for this cell
-
             angles = anglesMatrix{a}{row, col};
 
             % Loop over each angle and draw the lines
@@ -64,7 +71,7 @@ for a = 1:2
                 lineCoords = [-dx, dy; dx, -dy]'; % Column vector for x and y coordinates
 
                 % Draw the line
-                Screen('DrawLines', window, lineCoords, lineWidth, 0, [xCenter, yCenter], 2);
+                Screen('DrawLines', window, lineCoords, lineWidth, 0, [x_center, y_center], 2);
             end
         end
     end
@@ -75,7 +82,7 @@ for a = 1:2
     KbStrokeWait;
 
     current_display = Screen('GetImage', window);
-    name = strcat('C:\Users\idm\Desktop\Semester4\Internship\Matlab\example_', int2str(a), '.jpg');
+    name = strcat('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\Images\example_jittered', int2str(a), '.jpg');
     imwrite(current_display, name);
 end
 % Clear the screen
