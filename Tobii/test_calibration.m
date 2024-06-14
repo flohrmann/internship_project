52bfbb35df989157b6acd0c6abd738d7bb35b048
fani <96381820+flohrmann@users.noreply.github.com>
2024-06-14 12:45:00 +0200

    changed titta function to display text correcly

--- Tobii/test_calibration.m
+++ Tobii/test_calibration.m
@@ -1,12 +1,13 @@
-
+% ONLY WORKS WITH SKIPSYNCTESTS DISABLED
 screenNumber = 0; % 0 default screen, 1 for external screen
 color_bg = WhiteIndex(screenNumber);% colors for background and stim
 subfolder_name = 'C:\Users\flohrmann\Documents\MATLAB\internship_project\test';
-Screen('Preference', 'SkipSyncTests', 1); % TODO disable, skips synchronization tests
+%Screen('Preference', 'SkipSyncTests', 1); % TODO disable, skips synchronization tests
 [window, ~] = PsychImaging('OpenWindow', screenNumber, color_bg);
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
-
-%% Initialize Titta and set up the eye tracker 
+%screenXpixels = screenXpixels\2;
+%screenYpixels = screenYpixels\2;
+% Initialize Titta and set up the eye tracker 
 try
     settings = Titta.getDefaults('Tobii Pro Nano');
     eye_tracker = Titta(settings);
@@ -14,14 +15,14 @@ try
 catch
     error('No eye trackers found. Make sure the eye tracker is connected and try again.');
 end
-%% calibrate & safe results
-try
+% calibrate & safe results
+%try
     calibration = eye_tracker.calibrate(window);
     calibration_file = [subfolder_name, '\calibration_results.mat'];
     save(calibration_file, 'calibration');
-catch
-    error('Eye tracker couldnt be calibrated. Try again or restart experiment without eye tracking.');
-end
+%catch
+%    error('Eye tracker couldnt be calibrated. Try again or restart experiment without eye tracking.');
+%end
 % for testing only: fanis calibration
 % retrieve_calibration_data('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\results\calibration_results_20240513_1144')	;
 % apply_calibration_data(calibration_file);	
