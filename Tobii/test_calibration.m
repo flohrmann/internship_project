f4bc2c1deb888012d31b314c475813c380135c08
fani <96381820+flohrmann@users.noreply.github.com>
2024-06-14 13:08:13 +0200

    Update test_calibration.m

--- Tobii/test_calibration.m
+++ Tobii/test_calibration.m
@@ -8,21 +8,21 @@ subfolder_name = 'C:\Users\flohrmann\Documents\MATLAB\internship_project\test';
 %screenXpixels = screenXpixels\2;
 %screenYpixels = screenYpixels\2;
 % Initialize Titta and set up the eye tracker 
-try
+%try
     settings = Titta.getDefaults('Tobii Pro Nano');
     eye_tracker = Titta(settings);
     eye_tracker.init();
-catch
-    error('No eye trackers found. Make sure the eye tracker is connected and try again.');
-end
+%catch
+%    error('No eye trackers found. Make sure the eye tracker is connected and try again.');
+%end
 % calibrate & safe results
-%try
+try
     calibration = eye_tracker.calibrate(window);
     calibration_file = [subfolder_name, '\calibration_results.mat'];
     save(calibration_file, 'calibration');
-%catch
-%    error('Eye tracker couldnt be calibrated. Try again or restart experiment without eye tracking.');
-%end
+catch
+    error('Eye tracker couldnt be calibrated. Try again or restart experiment without eye tracking.');
+end
 % for testing only: fanis calibration
 % retrieve_calibration_data('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\results\calibration_results_20240513_1144')	;
 % apply_calibration_data(calibration_file);	
