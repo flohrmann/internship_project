7b72f170fefa7adf21617d6648d6711aa9034b74
fani <96381820+flohrmann@users.noreply.github.com>
2024-06-30 18:41:09 +0200

    changed language to test

--- testRun.m
+++ testRun.m
@@ -1,12 +1,18 @@
 %% Load custom path for testing experiment
 %data_struct = load('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\test\rand_trials.mat');
 %folder_name = 'C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\test';
+rand('state',42); % seed for reproducibility
+global exp_folder backup_folder ptb_drawformattedtext_oversize
+exp_folder = 'C:\Users\flohrmann\Documents\MATLAB\internship_project';
+results_folder = 'C:\Users\flohrmann\Documents\Results';
+backup_folder = 'C:\Users\flohrmann\Documen ts\Backup';
+ptb_drawformattedtext_oversize = 2;
 
 data_struct = load('C:\Users\flohrmann\Documents\MATLAB\internship_project\test\rand_trials.mat');
 folder_name = 'C:\Users\flohrmann\Documents\MATLAB\internship_project\test';
 
 data = data_struct.rand_trials;
-results = startPsychToolboxEyeTracking(data, folder_name, 9, 12, 10, 2);
+results = startPsychToolboxEyeTracking(data, folder_name, 9, 12, 10, 1);
 
 %% Fix fixation data
 data_struct = load('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\test\results\trial_results_with_fix.mat');
