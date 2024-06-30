5cbe5d69ef1bac5f2ea2f1429811811e41fe4784
fani <96381820+flohrmann@users.noreply.github.com>
2024-06-30 18:44:03 +0200

    changed dot size

--- startPsychToolboxEyeTracking.m
+++ startPsychToolboxEyeTracking.m
@@ -1,9 +1,11 @@
-function trial_results = startPsychToolboxEyeTracking(data, folder_name, NbX, NbY, timeout, language)
-% starts experiment and eyetracking
-% call with main
+
+function results = startPsychToolboxEyeTracking(data, folder_name, NbX, NbY, timeout, language)
+global exp_folder
+global ptb_drawformattedtext_oversize
 
 % make subfolder for results
 subfolder_name = [folder_name '\results'];
+disp(subfolder_name)
 mkdir(subfolder_name);
 % Define key names
 KbName('UnifyKeyNames');
@@ -22,13 +24,13 @@ bar_wh_ratio = 0.08; % bar width to height ratio
 screenNumber = 0; % 0 default screen, 1 for external screen
 color_bg = WhiteIndex(screenNumber);% colors for background and stim
 color_stim = BlackIndex(screenNumber);
-dotSizePix = 8; % Size of fixation point
+dotSizePix = 15; % Size of fixation point
 text_size = 30;
 
 % Open Psychtoolbox window
 %PsychDebugWindowConfiguration(0,0.5); % TODO disable, makes window transparent
-Screen('Preference', 'SkipSyncTests', 1); % TODO disable, skips synchronization tests
-[window, ~] = PsychImaging('OpenWindow', screenNumber, color_bg);
+%Screen('Preference', 'SkipSyncTests', 1); % TODO disable, skips synchronization tests
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, color_bg);
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 xCenter = screenXpixels / 2;
 yCenter = screenYpixels / 2;
@@ -49,38 +51,49 @@ Screen('TextColor', window, color_stim);
 % Enable alpha blending for anti-aliasing
 Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-%% Instructions
-texts_en = {'Welcome :) \n\n\n Press the Right Shift Key to Continue (and the left Shift Key to go Back to re-read instructions)', ...
-    'In this experiment you will be presented a grid of bars \n\n Press the Left Shift Key if you can see a uniquely oriented bar on the Left side of the screen \n and the Right Shift Key if you can see a uniquely oriented bar on the Right side of the screen', ...
-    'Lets look at a possible images you will see later', ...
-    'The trials can look like this for example. Can you find the uniquely oriented bar?',...
-    'The trials can also look like this. Can you still see the uniquely oriented bar?', ...
-    'Before each trial you will see a fixation dot in the middle of the screen, please remember to look at it for as long as it is there \n\n The next trial will only start if you look at it for around 2 seconds. \n This means that if you need a break you can just look away from the screen!', ...
-    'During the experiment I will also track your gaze, so lets calibrate the eyetracker first \n\n The eye tracking only works properly if you keep your head in the same position afterwards, \n so please make sure youre comfortable and can hold the position for the next 20 minutes!', ...
-    'Remember: your goal is to find the uniquely oriented bar and press the corresponding key' ...
-    'During the experiment once you have found the uniquely oriented bar remember to press the corresponding button \n\n Please try to be as fast and as accurate as possible!', ...
-    'Do you have any questions? \n \n If you feel ready: Press the Right Arrow Key To Begin The Experiment'};
-texts_ger = {'Willkommen :)', ...
-    'In diesem Experiment wird Ihnen ein Gitter aus Balken präsentiert. \n\n Drücken Sie die linke Shift Taste, wenn Sie links auf dem Bildschirm einen einzigartig orientierten Balken sehen können, \n und die rechte Shift-Taste, wenn Sie rechts auf dem Bildschirm einen einzigartig orientierten Balken sehen können', ...
-    'Lassen Sie uns einige mögliche Bilder anschauen, die Sie später sehen werden', ...
-    'Ein Durchlauf könnte könnte zum Beispiel so aussehen. Können Sie den einzigartig orientierten Balken finden?', ...
-    'Ein Durchlauf könnte auch so aussehen. Können Sie den einzigartig orientierten Balken immer noch finden?', ...
-    'Vor jedem Versuch sehen Sie einen Fixierungspunkt in der Mitte des Bildschirms, bitte schauen Sie so lange darauf, wie er dort ist \n\n Der nächste Versuch beginnt automatisch nach einer halben Sekunde.', ...
-    'During the experiment I will also track your gaze, so lets calibrate the eyetracker first \n\n The eye tracking only works properly if you keep your head in the same position afterwards, \n so please make sure youre comfortable and can hold the position for the next 20 minutes!', ...
-    'Während des Experiments, sobald Sie den einzigartig orientierten Balken gefunden haben, denken Sie daran, die entsprechende Taste zu drücken \n (linke/rechte Shift-Taste) \n Bitte versuchen Sie, so schnell und so genau wie möglich zu sein!', ...
-    'Haben Sie irgendwelche Fragen? \n \n Wenn Sie bereit sind: Drücken Sie die rechte Shift-Taste, um mit dem Experiment zu beginnen'};
 
-if language == 2 % choose instructions language
-    texts = texts_ger;
+%% Instructions & calibration
+if language == 1 % choose instructions language
+    text_path = strcat(exp_folder, '\Images\instructions_en');
 else
-    texts = texts_en;
+    text_path = strcat(exp_folder, '\Images\instructions_ger');
 end
 
 
-%% TODO add small instructions at beginning 
+% Show the first 2 pictures/instructions
+t = 1;
+while t <= 2
+    % Display first two images based on the current index
+    img = imread(strcat(text_path, sprintf('\\instructions-0%0d.jpg', t)));
+    displayImage(window, img, screenXpixels, screenYpixels);
+    while 1 % Check for key presses to navigate through texts
+        [~, ~, keyCode] = KbCheck;
+        if keyCode(rightKey) % Check for right arrow press
+            if t == 2 % Check if it's the second image
+                WaitSecs(0.2); % Prevent rapid key presses
+                t = t + 1; % Move index beyond the second to exit the loop
+                break; % Exit the inner while loop
+            else
+                t = t + 1; % Otherwise, just move to the next image
+                WaitSecs(0.2);
+                break;
+            end
+        elseif keyCode(leftKey) && t > 1
+            t = t - 1; % Navigate to the previous image
+            WaitSecs(0.2);
+            break;
+        elseif keyCode(escapeKey)
+            t = 3; % Move index beyond the second to exit the loop
+            break; % Exit the loop if the escape key is pressed
+        end
+    end
+    if t > 2
+        break; % Break the outer loop if past the second image
+    end
+end
 
 
-%% Initialize Titta and set up the eye tracker 
+% Initialize Titta and set up the eye tracker 
 try
     settings = Titta.getDefaults('Tobii Pro Nano');
     eye_tracker = Titta(settings);
@@ -88,7 +101,7 @@ try
 catch
     error('No eye trackers found. Make sure the eye tracker is connected and try again.');
 end
-%% calibrate & safe results
+% calibrate & safe results
 try
     calibration = eye_tracker.calibrate(window);
     calibration_file = [subfolder_name, '\calibration_results_' , datestr(now, 'yyyymmdd_HHMM'), '.mat'];
@@ -96,48 +109,36 @@ try
 catch
     error('Eye tracker couldnt be calibrated. Try again or restart experiment without eye tracking.');
 end
-% for testing only: fanis calibration
-% retrieve_calibration_data('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\results\calibration_results_20240513_1144')	;
-% apply_calibration_data(calibration_file);	
 
-%% show instructions
-t = 1;
-while t <= numel(texts)
-    % Display text or image based on the current index
-    if t == 4 % Add images to certain 'slides'
-        img = imread('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\Images\example_jittered1.jpg'); 
-        displayImage(window, texts{t}, img, screenXpixels, screenYpixels);
-    elseif t == 5
-        img = imread('C:\Users\idm\Desktop\Semester4\Internship\Matlab\ExperimentRepo\Images\example_jittered2.jpg'); 
-        displayImage(window, texts{t}, img, screenXpixels, screenYpixels);
-    else
-        displayText(window, texts{t}); % Display text only
+% show rest of instructions
+image_number = 3; % Start from the third image
+while image_number <= 11 % until picture 11    
+    try
+        img = imread(strcat(text_path, sprintf('\\instructions-0%0d.jpg', image_number)));
+    catch
+        break; % Exit the loop if no more images are found
     end
-    while 1 % Check for key presses to navigate through texts
+    displayImage(window, img, screenXpixels, screenYpixels);
+    while 1 % Check for key presses to navigate through images
         [~, ~, keyCode] = KbCheck;
         if keyCode(rightKey) % Check for right arrow press
-            if t == numel(texts) % Check if it's the last text
-                WaitSecs(0.2); % Prevent rapid key presses
-                t = t + 1; % Move index beyond the last to exit the loop
-                break; % Exit the inner while loop
-            else
-                t = t + 1; % Otherwise, just move to the next text
-                WaitSecs(0.2); 
-                break; 
-            end
-        elseif keyCode(leftKey) && t > 1
-            t = t - 1; % Navigate to the previous text
-            WaitSecs(0.2); 
-            break; 
+            image_number = image_number + 1; % Move to the next image
+            WaitSecs(0.2); % Prevent rapid key presses
+            break;
+        elseif keyCode(leftKey) && image_number > 3
+            image_number = image_number - 1; % Navigate to the previous image
+            WaitSecs(0.2);
+            break;
         elseif keyCode(escapeKey)
-            t = numel(texts) + 1; % Move index beyond the last to exit the loop
+            image_number = inf; % Move index beyond the last to exit the loop
             break; % Exit the loop if the escape key is pressed
         end
     end
-    if t > numel(texts) 
-        break; % Break the outer loop if past the last text
+    if image_number == inf
+        break; % Break the outer loop if escape key was pressed
     end
 end
+
 
 
 %% Experiment loop starts here
@@ -148,18 +149,37 @@ trial_results = table();
 % TODO disable/change back
 % for i = 1:size(data, 1)
 for i = 1:4
+%     % Display fixation dot & empty screen after
+%     fixStartTime = GetSecs;
+%     % Define the area around the fixation point where gaze must fall
+%     fixationRadius = 50; % pixels, defines a circle radius around the fixation point
+%     minFixationTime = 1.0; % seconds, the minimum time gaze must remain within the radius
+%     samp_fix = drawFixationAndWait(window, xCenter, yCenter, color_stim, dotSizePix, eye_tracker, screenXpixels, screenYpixels, fixationRadius, minFixationTime);
+%         %drawFixation(window, xCenter, yCenter, color_stim, dotSizePix); %without eyetrackgin
+%         %WaitSecs(0.5); 
+%     fixEndTime = GetSecs;
+%     drawEmptyScreen(window, color_bg, 1)
+%     WaitSecs(0.2); % wait before drawing stim
+% %     end
+
     % Display fixation dot & empty screen after
     fixStartTime = GetSecs;
-    % Define the area around the fixation point where gaze must fall
-    fixationRadius = 50; % pixels, defines a circle radius around the fixation point
-    minFixationTime = 1.0; % seconds, the minimum time gaze must remain within the radius
-    samp_fix = drawFixationAndWait(window, xCenter, yCenter, color_stim, dotSizePix, eye_tracker, screenXpixels, screenYpixels, fixationRadius, minFixationTime);
-        %drawFixation(window, xCenter, yCenter, color_stim, dotSizePix); %without eyetrackgin
-        %WaitSecs(0.5); 
+    eye_tracker.buffer.start('gaze');
+    Screen('TextSize', window, text_size);
+    displayText(window, 'Press any button to continue with the next trial.');
+    while true
+    [~, ~, keyCode] = KbCheck;
+        if keyCode(leftKey) || keyCode(rightKey)
+            break;
+        end
+    end
+    Screen('Flip', window);
+    drawFixation(window, xCenter, yCenter, color_stim, dotSizePix); %without eyetrackgin
+    WaitSecs(0.5); 
     fixEndTime = GetSecs;
     drawEmptyScreen(window, color_bg, 1)
     WaitSecs(0.2); % wait before drawing stim
-%     end
+    samp_fix = eye_tracker.buffer.consumeN('gaze');
 
     current_data = data(i,:);
     % Start recording eye data
@@ -167,39 +187,38 @@ for i = 1:4
     % Display stimulus
     trial_result = displayStim_new(window, current_data, bar_wh_ratio, jitter_x, jitter_y, ...
         screenXpixels, screenYpixels, color_stim, timeout);
-    
-    % Get eye data, since start/last getting eye data & safe
+        % Get eye data, since start/last getting eye data & safe
     samp = eye_tracker.buffer.consumeN('gaze');
-    
-    trial_result.samp = samp; % add eye data
 
     % Add data from fixation
     trial_result.fixStartTime = fixStartTime;
     trial_result.fixEndTime = fixEndTime;
     trial_result.fixTime = fixEndTime - fixStartTime;
-    trial_result.sampFix = {samp_fix}; 
+    trial_result.samp_fix = samp_fix; 
+
+    trial_result.samp = samp; % add eye data
 
     % save/overwrite each loop in case experiment crashes/gets aborted
     trial_results(i,:) = trial_result(1,:);
     save(results_file_name, 'trial_results');
-
+    
     % Check for Escape key press to close the experiment
     if keyCode('ESCAPE')
         disp('Experiment Aborted: Escape Key was pressed')
         break;
     end
 end
-
-%% End
 % Clean up: stop eye tracker, save results, and close
 eye_tracker.buffer.stop('gaze');
 eye_tracker.deInit();
-
+results = trial_results;
+%% End
 % End screen
 % TODO CHANGE BACK FOR EXPERIMENT
 %if i == size(data, 1)
 if i == 4
-    if language == '2' % choose instructions language
+    Screen('TextSize', window, text_size);
+    if language == '2' % choose instructions language     
         displayText(window, 'Experiment Abgeschlossen\n\n Vielen Dank fürs Mitmachen! <3');
     else
         displayText(window, 'Experiment Complete\n\n Thank you for your participation! <3');
@@ -207,6 +226,7 @@ if i == 4
     WaitSecs(2);
     KbStrokeWait(-1);  % Wait to end with key press
 end
+
 disp('Experiment Finished')
 sca; % Close the window
 end
