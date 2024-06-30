4cb57ab7405ae3c136397ae993b04dc619857ecc
fani <96381820+flohrmann@users.noreply.github.com>
2024-06-30 18:42:20 +0200

    changed sizes

--- displayImage.m
+++ displayImage.m
@@ -1,12 +1,12 @@
-function displayImage(window, text, img, screenXpixels, screenYpixels)
+function displayImage(window, img, screenXpixels, screenYpixels)
         texture = Screen('MakeTexture', window, img);
         % Calculate the position of the scaled image to center it
-        dstX = (screenXpixels - screenXpixels * 0.7) / 2;
-        dstY = (screenYpixels - screenYpixels * 0.7) / 2;
-        dstRect = [dstX dstY dstX + screenXpixels * 0.7 dstY + screenYpixels * 0.7];
+        dstX = (screenXpixels - screenXpixels) / 2;
+        dstY = (screenYpixels - screenYpixels) / 2;
+        dstRect = [dstX dstY dstX + screenXpixels dstY + screenYpixels];
         % Add text on top of the image
         %Screen('TextSize', window, 30);
-        DrawFormattedText(window, text, 'center', dstY * 0.8, 0);
+        %DrawFormattedText(window, text, 'center', dstY * 0.8, 0);
         % Draw the scaled texture to the window
         Screen('DrawTexture', window, texture, [], dstRect);
         %Screen('DrawTexture', window, texture, []);
